560897e499c0f019d2cca45747eff2a26d6682ed
ThomasShumanJr <Ts061292>
2019-06-23 03:27:30 -0500

    Forgot to set this dmaned thing up for some god awful reason, call me an idiot later self

--- app/src/main/java/com/deadmandev/tabletopassisstant/controllers/App.kt
+++ app/src/main/java/com/deadmandev/tabletopassisstant/controllers/App.kt
@@ -8,7 +8,9 @@ class App: Application() {
     companion object {
         lateinit var sharedPreferences: SharedPreferences
     }
+
     override fun onCreate() {
         sharedPreferences = SharedPreferences(applicationContext)
         super.onCreate()
     }
+}
